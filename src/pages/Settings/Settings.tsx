672c17d311c8138d85151be5e3fb91fa97257cf6
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2024-12-13 06:23:20 +0530

    feat: settings

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import FileReaderInput, { Result } from 'react-file-reader-input'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
